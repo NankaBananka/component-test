fa7cc99f6994fa95dad70b9520fd1e118ac47b39
Nelya <nailia.shakirova@gmail.com>
2018-09-04 22:12:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import os
 print("root", os.listdir("/data"))
 print("root/repository", os.listdir("/data/repository"))
+print("in", os.listdir("/data/in"))
+print("out", os.listdir("/data/out"))
 
 
 print("parent", os.listdir("../"))
 
-for i in os.listdir("../"):
-  print(i, os.path.isdir(i))
-  if os.path.isdir(i):
-    print(os.listdir())
+#for i in os.listdir("../"):
+#  print(i, os.path.isdir(i))
+#  if os.path.isdir(i):
+#    print(os.listdir())
 
 
 
-print("parent", os.listdir("../../"))
+#print("parent", os.listdir("../../"))
