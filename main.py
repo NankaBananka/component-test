31a10a1db449f16b845650bc59fef41c8049858e
Nelya <nailia.shakirova@gmail.com>
2018-09-04 22:06:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import os
 print("root", os.listdir("/data"))
 print("root/repository", os.listdir("/data/repository"))
-list_dir = os.listdir()
 
-for i in list_dir:
-  print(os.path.isdir(i), i)
 
 print("parent", os.listdir("../"))
 
