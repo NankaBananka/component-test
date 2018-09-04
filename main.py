9ca588d7d91ad453c4f75b523a6c79d723d207e8
Nelya <nailia.shakirova@gmail.com>
2018-09-04 16:40:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ url = 'http://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.c
 r = requests.get(url)
 
 #create or open (if exist) output file for writing and write the content of the response
-with open('out/tables/titanic.csv', 'wb') as f:  
+with open('/data/out/tables/titanic.csv', 'wb') as f:  
     f.write(r.content)
 
 #read output file and print to console log first 10 rows
-data = pd.read_csv('out/tables/titanic.csv', header=0)
+data = pd.read_csv('/data/out/tables/titanic.csv', header=0)
 print(data.loc[0:10,["Name", "Survived"]])
