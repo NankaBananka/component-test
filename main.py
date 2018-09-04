9fc32756c44366fbcaa42578287e9b80c99aefcd
Nelya <nailia.shakirova@gmail.com>
2018-09-04 16:44:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ print(os.getcwd())
 
 
 #import necessary libraries
-import requests
-import pandas as pd
+#import requests
+#import pandas as pd
 
 #request url and save response to variable
-url = 'http://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv'
-r = requests.get(url)
+#url = 'http://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv'
+3r = requests.get(url)
 
 #create or open (if exist) output file for writing and write the content of the response
-with open('/data/out/tables/titanic.csv', 'wb') as f:  
-    f.write(r.content)
+#with open('/data/out/tables/titanic.csv', 'wb') as f:  
+#    f.write(r.content)
 
 #read output file and print to console log first 10 rows
-data = pd.read_csv('/data/out/tables/titanic.csv', header=0)
-print(data.loc[0:10,["Name", "Survived"]])
+#data = pd.read_csv('/data/out/tables/titanic.csv', header=0)
+#print(data.loc[0:10,["Name", "Survived"]])
