5c4f97e13f432695cb34093a259c4b3f6ca3fb65
Nelya <nailia.shakirova@gmail.com>
2018-09-10 21:18:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 #import necessary libraries
-import requests
-import pandas as pd
+import requests, json, pandas as pd
 
-#request url and save response to variable
-url = 'http://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv'
+#obtain url property from config.json file
+with open('/data/config.json') as f:
+    json_data = json.load(f)
+    url = json_data['parameters']['url']
+    
+#request url and save response into variable
 r = requests.get(url)
 
-#create or open (if exist) output file for writing and write the content of the response
-with open('/data/out/tables/titanic.csv', 'wb') as f:  
+#create or open (if exists) output file for writing and write the content of the response
+with open('/data/out/tables/titanic.csv', 'wb') as f:
     f.write(r.content)
 
 #read output file and print to console log first 10 rows
 data = pd.read_csv('/data/out/tables/titanic.csv', header=0)
+
 print(data.loc[0:10,["Name", "Survived"]])
