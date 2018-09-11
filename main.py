213bc8797cb001e1e576189e9c02ab4e00b8423b
Nelya <nailia.shakirova@gmail.com>
2018-09-11 10:25:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ with open('/data/config.json') as f:
 #request url and save response into variable
 r = requests.get(url)
 
+full_path = '/data/out/tables/' + path + name
+
 #create or open (if exists) output file for writing and write the content of the response
-with open('/data/out/tables/titanic.csv', 'wb') as f:
+with open(full_path, 'wb') as f:
     f.write(r.content)
 
 #read output file and print to console log first 10 rows
-data = pd.read_csv('/data/out/tables/titanic.csv', header=0)
+data = pd.read_csv(full_path, header=0)
 
 print(data.loc[0:10,["Name", "Survived"]])
