c57bd368775aaeae9db92f92fca633adc7594a88
Nelya <nailia.shakirova@gmail.com>
2018-09-11 10:54:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ with open('/data/config.json') as f:
 #request url and save response into variable
 r = requests.get(url)
 
-full_path = '/data/out/tables/' + path
-Path(full_path).mkdir(parents=True, exist_ok=True)
+full_path = '/data/out/tables/' + path + "/" + name
+Path('/data/out/tables/' + path).mkdir(parents=True, exist_ok=True)
 
 #create or open (if exists) output file for writing and write the content of the response
-with open(full_path + "/" + name, 'wb') as f:
+with open(full_path, 'wb') as f:
     f.write(r.content)
 
 #read output file and print to console log first 10 rows
